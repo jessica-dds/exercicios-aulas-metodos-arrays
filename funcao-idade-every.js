6da22f30299bdeac5674b7499e8dabdc8f4b2887
jessica-dds <jessica.gleizer@hotmail.com>
2024-01-03 18:50:54 -0300

    every

---
+++ funcao-idade-every.js
@@ -0,0 +1,20 @@
+const usuarios = [
+    { nome: 'joao', idade: 23 },
+    { nome: 'maria', idade: 18 },
+    { nome: 'ana', idade: 30 },
+    { nome: 'rodrigo', idade: 19 }
+];
+
+const fiscalizarFesta = (arrayObjetos) => {
+    const resultado = arrayObjetos.every((objeto) => {
+        return objeto.idade > 17;
+    });
+
+    if (resultado) {
+        console.log("Festa liberada.")
+    } else {
+        console.log("Possui menor de idade.")
+    }
+}
+
+fiscalizarFesta(usuarios);
