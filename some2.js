bf81e3c79555441a8b2fc786cd6ca6e7132f37d8
jessica-dds <jessica.gleizer@hotmail.com>
2024-01-03 19:26:28 -0300

    some2

---
+++ some2.js
@@ -0,0 +1,21 @@
+const arquivos = [
+    'teste.bat',
+    'foto.png',
+    'contrato.doc',
+    'instalador.exe'
+];
+
+const antiVirus = (arrayArquivos) => {
+    const resultado = arrayArquivos.some((arquivo) => {
+        const existeExtensao = arquivo.indexOf('.bat');
+        return existeExtensao !== -1;
+    });
+
+    if (resultado) {
+        console.log("Vírus detectado!")
+    } else {
+        console.log("Nenhum vírus encontrado!")
+    }
+}
+
+antiVirus(arquivos);
