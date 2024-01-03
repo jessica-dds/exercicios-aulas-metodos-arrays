d7ba55bb97142e9f2a8594843476e5ef7c1f9daf
jessica-dds <jessica.gleizer@hotmail.com>
2024-01-03 15:42:40 -0300

    Callbacks

---
+++ callbacks.js
@@ -0,0 +1,13 @@
+const imprimirJessica = () => {
+    console.log('Jessica Gleizer')
+}
+
+//setTimeout(imprimirJessica, 5000) //imprimir depois de 5 segundos
+
+//setTimeout(() => {
+//   console.log('Cubos Academy')
+//}, 5000)
+
+setTimeout(function () {
+    console.log('Cubos Academy')
+}, 5000)
