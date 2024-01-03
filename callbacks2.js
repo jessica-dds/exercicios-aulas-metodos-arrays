02776281ac3e52bb19c6f3ca9c99da1013242237
jessica-dds <jessica.gleizer@hotmail.com>
2024-01-03 16:59:21 -0300

    callback contador

---
+++ callbacks2.js
@@ -0,0 +1,14 @@
+//const imprimir = () => {
+//   console.log('Olá, 2024!')
+//    clearInterval(interval)
+//}
+//
+//const interval = setInterval(imprimir, 2000)
+
+
+let numero = 0
+
+const contador = setInterval(function () {
+    console.log(numero)
+    numero++
+}, 1000)
