602ea4bf06d30fb23d1227e45c9a81ebcb0d4477
jessica-dds <jessica.gleizer@hotmail.com>
2024-01-09 12:06:59 -0300

    Reduce

---
+++ reduce.js
@@ -0,0 +1,26 @@
+const array = [0, 1, 2, 3, 4];
+
+
+// Como somar todos dos números do array usando o for 
+
+// let somaTotal = array[0];
+
+// for (let i = 1; i < array.length; i++) {
+//     const elementoAtual = array[i];
+
+//     somaTotal = somaTotal + elementoAtual;
+// }
+
+// console.log(somaTotal);
+
+// const valorReduce = array.reduce((acumulador, elementoAtual, indice, array) => {
+//     return acumulador + elementoAtual;
+// });
+
+// console.log(valorReduce);
+
+const valorReduce2 = array.reduce((acumulador, elementoAtual, indice, array) => {
+    return acumulador + elementoAtual;
+}, 10);
+
+console.log(valorReduce2);
